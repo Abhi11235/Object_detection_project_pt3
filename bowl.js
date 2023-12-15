b933e439e17e9cd00c878442cd41627b1ce4d649
Abhi11235 <117432986+Abhi11235@users.noreply.github.com>
2023-12-15 17:02:20 +1100

    Add files via upload

---
+++ bowl.js
@@ -0,0 +1,66 @@
+img="";
+Status="";
+objects=  [];
+
+function preload(){
+img=loadImage("bowl.jpg");
+}
+
+
+function setup() {
+    canvas=createCanvas(640, 420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+  document.getElementById("status").innerHTML= "status: Detecting Objects"; 
+}
+
+
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+     Status=true;
+     objectDetector.detect(video, gotResults);
+}
+
+
+
+function gotResults(error,results){
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
+}
+
+function draw() {
+    image(video,0,0,380,380);
+    fill("orange");
+    text("bowl",45,75);
+    noFill();
+    stroke("orange");
+    rect(30,60,450,350);
+
+    
+   
+    
+    
+    if(Status !="")
+    {
+        objectDetector.detect(video, gotResults);
+        r= random(255);
+       g= random(255);
+        b=random(255);
+    for (i=0; i < objects.legnth; i++)
+    {
+        document.getElementById("status").innerHTML = "Status: Object detected";
+        document.getElementById("Number of Objects").innerHTML = "Number of objects detected are:"
+        +objects.length;
+    }
+
+   
+}
+}
+
+
+
+    
